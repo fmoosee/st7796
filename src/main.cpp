8775bb48be963b36180f6e3e9d9ea958ebf29379
fmoosee <juliorita67@gmail.com>
2025-02-14 23:30:03 -0300

    14/02 v2

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,9 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
   }
 }
 
+LV_FONT_DECLARE(fontClock);
 lv_obj_t* menuScr;
+int32_t var = 1;
 
 void setup()
 {
@@ -74,49 +76,11 @@ void setup()
   lv_indev_drv_register(&indev_drv);
   menuScr = lv_obj_create(NULL);
   lv_scr_load(menuScr);
-  lv_obj_t* center = lv_obj_create(menuScr);
-  lv_obj_center(center);
-  lv_obj_set_style_radius(center, LV_RADIUS_CIRCLE, 0);
-  lv_obj_set_size(center, 100, 100);
-  lv_obj_t* label = lv_label_create(center);
-  LV_FONT_DECLARE(fontClock);
-  lv_obj_set_style_text_font(label, &fontClock, 0);
-  lv_obj_center(label);
-  lv_label_set_text(label, "0");
-  static int i = 0;
-
-  lv_obj_add_event_cb(center, [] (lv_event_t* e){
-    lv_event_code_t event = lv_event_get_code(e);
-    lv_obj_t* obj = lv_event_get_target(e);
-    lv_obj_t* child = lv_obj_get_child(obj, 0);
-    if(event == LV_EVENT_CLICKED){
-      i++;
-      lv_label_set_text_fmt(child, "%d", i); 
-    }
-  }, LV_EVENT_ALL, NULL);
-
-  lv_obj_t* btn = lv_btn_create(menuScr);
-  lv_obj_align(btn, LV_ALIGN_RIGHT_MID, -10, 0);
-  lv_obj_set_size(btn, 100, 50);
-
-  lv_obj_add_event_cb(btn, [] (lv_event_t* e){
-    lv_obj_t* obj = (lv_obj_t*)e->user_data;
-    lv_obj_t* child = lv_obj_get_child(obj, 0);
-    lv_label_set_text(child, "0");
-    i = 0;
-    lv_anim_t a;
-    lv_anim_init(&a);
-    lv_anim_set_exec_cb(&a, [](void* obj, int32_t val){
-      lv_obj_t* item = (lv_obj_t*)obj;
-      lv_obj_set_size(item, val, val);
-    });
-    lv_anim_set_values(&a, 100, 105);
-    lv_anim_set_path_cb(&a, lv_anim_path_ease_in_out);
-    lv_anim_set_time(&a, 100);
-    lv_anim_set_playback_time(&a, 100);
-    lv_anim_set_var(&a, obj);
-    lv_anim_start(&a);
-  }, LV_EVENT_CLICKED, center);
+  lv_obj_t* numericVal = lv_obj_create(menuScr);
+  lv_obj_center(numericVal);
+  lv_obj_set_size(numericVal, 100, 100);
+  lv_obj_set_style_radius(numericVal, LV_RADIUS_CIRCLE, 0);
+  lv_obj_set_style_bg_grad_color(numericVal, lv_color_hex(0xffffff), 0);
 }
 
 void loop()
